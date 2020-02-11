2dafbd1b9db5263d0c65a87574a82129a9c05ba1
Kseniia Tereshenkova <ksenia24t@gmail.com>
2020-02-11 17:56:32 +0100

    eureka-server aufgesetzt

---
+++ eureka-service/src/test/java/hs/tasklistie/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.tasklistie.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
